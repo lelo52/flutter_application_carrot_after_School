e48f168f1d316b11e20ca20f8ea31caad90909f9
leelo52 <115298517+lelo52@users.noreply.github.com>
2023-06-14 17:38:30 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_carrot/detail_page.dart';
-import 'carrot_item.dart';
+import 'kobis_api.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  List<CarrotItem> items = [];
-  MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    items.add(CarrotItem(title: '파자팝니다', addr: '성남시 중원구', price: 1000000));
-    items.add(CarrotItem(title: '정묵팝니다', addr: '성남시 중원구', price: 1));
-    items.add(CarrotItem(title: '묵정팝니다', addr: '성남시 중원구', price: 10));
-    return MaterialApp(
-      home: HomePage(items: items),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({
+class MainPage extends StatefulWidget {
+  const MainPage({
     super.key,
-    required this.items,
   });
 
-  final List<CarrotItem> items;
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  final kobisApi = KobisApi(apiKey: '01736d1cbc4c7f3e45f3f5f6354d4e12');
+
+  void showCal() async {
+    var dt = await showDatePicker(context: context, 
+    initialDate: DateTime.now().subtract(const Duration(days: 1)), 
+    firstDate: DateTime(2022), 
+    lastDate: DateTime.now().subtract(const Duration(days: 1))
+    );
+    if(dt !=null){
+      //2022-02-02 00:00:00
+      var targetDt = dt.toString().split(' ')[0].replaceAll('-', '');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('jh_market'),
-        backgroundColor: Colors.cyanAccent,
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            for (var item in items)
-              GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DetailPage(
-                                  price: item.price,
-                                )));
-                  },
-                  child: item)
-          ],
-        ),
-      ),
+      floatingActionButton: FloatingActionButton(onPressed: ),
     );
   }
 }
